dc62bbcf31578bb99914f28eceab05cd2f473587
HM-Tanjil9 <rtanjil0@gmail.com>
2025-07-15 00:20:48 +0600

    Feature: Added mongoose db config

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const {PORT} = require('./config/server.config');
 const apiRouter = require('./routes');
 const BaseError = require('./errors/base.error');
 const errorHandler = require('./utils/errorHandler');
+const connectToDB = require('./config/db.config');
+const { default: mongoose } = require('mongoose');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -23,12 +25,9 @@ app.get('/ping', (req, res) => {
 // Last middleware if any error comes
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT,async () => {
     console.log(`Server started at PORT: ${PORT}`);
-    try {
-        //throw new NotFoundError({})
-    } catch(e) {
-        console.log("Something happened",e);
-        
-    }
+    await connectToDB();
+    console.log("Successfully connected to DB");
+
 });
